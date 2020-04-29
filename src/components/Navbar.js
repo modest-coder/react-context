f5db49d9b18e84802ee728fac7d7f54f81c7299e
PH <paulogomes.wd@gmail.com>
2020-04-29 10:09:08 +0100

    Consuming context through a static property

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
+import { ThemeContext } from '../contexts/ThemeContext';
 import React, { Component } from 'react';
 
 class Navbar extends Component {
+  static contextType = ThemeContext;
   render() {
+    // console.log(this.context);
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <nav>
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
         <h1>Context App</h1>
         <ul>
           <li>Home</li>
