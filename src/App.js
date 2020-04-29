03450afded16b98f7fd1c324c25c3ef91f6e47a4
PH <paulogomes.wd@gmail.com>
2020-04-29 11:17:05 +0100

    Consuming Multiple Contexts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ThemeContextProvider from './contexts/ThemeContext';
+import AuthContextProvider from './contexts/AuthContext';
 import ThemeToggle from './components/ThemeToggle';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
@@ -7,11 +8,13 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
-      </ThemeContextProvider>
+      <AuthContextProvider>
+        <ThemeContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </ThemeContextProvider>
+      </AuthContextProvider>
     </div>
   );
 }
